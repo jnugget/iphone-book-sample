8d41ab1040305ffdad2a2f5c8b450f7333578a2c
iphone@main-loop.com <iphone@main-loop.com@8f08c6d2-e6f8-11de-92aa-3ddf338522b3>
2010-02-14 01:16:42 +0000

    Chapter9 - TableSample カスタムセルの扱いを修正

--- Chapter9/TableSample/Classes/SampleForCustomizedCell.h
+++ Chapter9/TableSample/Classes/SampleForCustomizedCell.h
@@ -1,15 +1,66 @@
 //
 //  SampleForCustomizedCell.h
 //
-//  Created by ToKoRo on 2009-09-24.
+//  Created by ToKoRo on 2010-02-14.
 //
 
-#import <UIKit/UIKit.h>
+#pragma mark ----- CellWithImageView -----
 
-@interface SampleForCustomizedCell : UITableViewController
+@interface CellWithImageView : UITableViewCell
+{
+ @private
+  UIImageView* imageView_;
+}
+
+@property (nonatomic, retain) UIImageView* imageView;
+
+@end 
+
+#pragma mark ----- CellWithSwitch -----
+
+@interface CellWithSwitch : UITableViewCell
+{
+ @private
+  UISwitch* theSwitch_;
+}
+
+@property (nonatomic, retain) UISwitch* theSwitch;
+
+@end 
+
+#pragma mark ----- CellWithSlider -----
+
+@protocol CellWithSliderDelegate
+@required
+- (void)slider:(UISlider*)slider valueDidChange:(float)value forRow:(NSUInteger)row;
+@end 
+
+@interface CellWithSlider : UITableViewCell
+{
+ @private
+  UISlider* slider_;
+  NSUInteger row_;
+  id delegate_;
+}
+
+@property (nonatomic, retain) UISlider* slider;
+@property (nonatomic, assign) NSUInteger row;
+@property (nonatomic, assign) id delegate;
+
+@end 
+
+#pragma mark ----- SampleForCustomizedCell -----
+
+@interface SampleForCustomizedCell : UITableViewController <CellWithSliderDelegate>
 {
  @private
   NSArray* sections_;
   NSArray* dataSource_;
+  NSMutableArray* sliderValues_;
 }
+
+@property (nonatomic, retain) NSArray* sections;
+@property (nonatomic, retain) NSArray* dataSource;
+@property (nonatomic, retain) NSMutableArray* sliderValues;
+
 @end
--- Chapter9/TableSample/Classes/SampleForCustomizedCell.m
+++ Chapter9/TableSample/Classes/SampleForCustomizedCell.m
@@ -1,26 +1,131 @@
 //
 //  SampleForCustomizedCell.m
 //
-//  Created by ToKoRo on 2009-09-24.
+//  Created by ToKoRo on 2010-02-14.
 //
 
 #import "SampleForCustomizedCell.h"
 
-#pragma mark ----- Private Methods Definition -----
+#pragma mark ----- CellWithImageView -----
 
-@interface SampleForCustomizedCell ()
-- (UIImageView*)imageViewForCell:(const UIView*)cell withFileName:(NSString*)fileName;
-- (UISwitch*)switchForCell:(const UIView*)cell;
-- (UISlider*)sliderForCell:(const UIView*)cell;
+@implementation CellWithImageView
+
+@synthesize imageView = imageView_;
+
+- (void)dealloc {
+  self.imageView = nil;
+  [super dealloc];
+}
+
+- (id)initWithReuseIdentifier:(NSString*)identifier {
+  if ( (self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier]) ) {
+    self.imageView = [[[UIImageView alloc] init] autorelease];
+    self.imageView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
+                                      UIViewAutoresizingFlexibleRightMargin |
+                                      UIViewAutoresizingFlexibleTopMargin |
+                                      UIViewAutoresizingFlexibleBottomMargin;
+    [self addSubview:self.imageView];
+  }
+  return self;
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  [self.imageView sizeToFit];
+  CGPoint newCenter = self.contentView.center;
+  newCenter.x += 80;
+  self.imageView.center = newCenter;
+}
+
 @end 
 
-#pragma mark ----- Start Implementation For Methods -----
+#pragma mark ----- CellWithSwitch -----
+
+@implementation CellWithSwitch
+
+@synthesize theSwitch = theSwitch_;
+
+- (void)dealloc {
+  self.theSwitch = nil;
+  [super dealloc];
+}
+
+- (id)initWithReuseIdentifier:(NSString*)identifier {
+  if ( (self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier]) ) {
+    self.theSwitch = [[[UISwitch alloc] init] autorelease];
+    self.theSwitch.on = YES;
+    self.theSwitch.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
+                                      UIViewAutoresizingFlexibleRightMargin |
+                                      UIViewAutoresizingFlexibleTopMargin |
+                                      UIViewAutoresizingFlexibleBottomMargin;
+    [self addSubview:self.theSwitch];
+  }
+  return self;
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  CGPoint newCenter = self.contentView.center;
+  newCenter.x += 80;
+  self.theSwitch.center = newCenter;
+}
+
+@end 
+
+#pragma mark ----- CellWithSlider -----
+
+@implementation CellWithSlider
+
+@synthesize slider = slider_;
+@synthesize row = row_;
+@synthesize delegate = delegate_;
+
+- (void)dealloc {
+  self.slider = nil;
+  [super dealloc];
+}
+
+- (id)initWithReuseIdentifier:(NSString*)identifier {
+  if ( (self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier]) ) {
+    self.slider = [[[UISlider alloc] init] autorelease];
+    self.slider.frame = CGRectMake( 0, 0, 160, 20 );
+    self.slider.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
+                                   UIViewAutoresizingFlexibleRightMargin |
+                                   UIViewAutoresizingFlexibleTopMargin |
+                                   UIViewAutoresizingFlexibleBottomMargin;
+    [self.slider addTarget:self
+                    action:@selector(sliderValueDidChange:)
+          forControlEvents:UIControlEventValueChanged];
+    [self addSubview:self.slider];
+  }
+  return self;
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  CGPoint newCenter = self.contentView.center;
+  newCenter.x += 50;
+  self.slider.center = newCenter;
+}
+
+- (void)sliderValueDidChange:(UISlider*)slider {
+  [self.delegate slider:slider valueDidChange:slider.value forRow:self.row];
+}
+
+@end 
+
+#pragma mark ----- SampleForCustomizedCell -----
 
 @implementation SampleForCustomizedCell
 
+@synthesize sections = sections_;
+@synthesize dataSource = dataSource_;
+@synthesize sliderValues = sliderValues_;
+
 - (void)dealloc {
-  [sections_ release];
-  [dataSource_ release];
+  self.sections = nil;
+  self.dataSource = nil;
+  self.sliderValues = nil;
   [super dealloc];
 }
 
@@ -33,46 +138,77 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // 表示するデータを作成
-  sections_ = [[NSArray alloc] initWithObjects: @"名前", @"必殺技", @"強さ", nil ];
-  NSArray* rows1 = [NSArray arrayWithObjects: @"宮本武蔵", nil ];
-  NSArray* rows2 = [NSArray arrayWithObjects: @"二刀流", nil ];
-  NSArray* rows3 = [NSArray arrayWithObjects: @"攻撃力", @"防御力", nil ];
-  dataSource_ = [[NSArray alloc] initWithObjects: rows1, rows2, rows3, nil ];
+  self.sections = [NSArray arrayWithObjects:@"名前", @"必殺技", @"強さ", nil];
+  NSArray* rows1 = [[NSArray alloc] initWithObjects:@"宮本武蔵", nil];
+  NSArray* rows2 = [[NSArray alloc] initWithObjects:@"二刀流", nil ];
+  NSArray* rows3 = [[NSArray alloc] initWithObjects:
+                                             @"攻撃力", @"防御力",
+                                             @"etc1", @"etc2",
+                                             @"etc3", @"etc4",
+                                             @"etc5", @"etc6", 
+                                             @"etc7", @"etc8",
+                                             @"etc9", @"etc10", nil];
+  self.dataSource = [NSArray arrayWithObjects: rows1, rows2, rows3, nil];
+  self.sliderValues = [NSMutableArray arrayWithObjects:
+                                             [NSNumber numberWithFloat:0.9], [NSNumber numberWithFloat:0.8],
+                                             [NSNumber numberWithFloat:0.1], [NSNumber numberWithFloat:0.2],
+                                             [NSNumber numberWithFloat:0.3], [NSNumber numberWithFloat:0.4],
+                                             [NSNumber numberWithFloat:0.5], [NSNumber numberWithFloat:0.6],
+                                             [NSNumber numberWithFloat:0.7], [NSNumber numberWithFloat:0.8],
+                                             [NSNumber numberWithFloat:0.9], [NSNumber numberWithFloat:1.0], nil];
+  [rows1 release];
+  [rows2 release];
+  [rows3 release];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-  return [sections_ count];
+  return [self.sections count];
 }
 
 - (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section {
-  return [[dataSource_ objectAtIndex:section] count];
+  return [[self.dataSource objectAtIndex:section] count];
 }
 
 - (UITableViewCell*)tableView:(UITableView*)tableView
   cellForRowAtIndexPath:(NSIndexPath*)indexPath
 {
-  static NSString* identifier = @"basis-cell";
+  // カスタムセルの種類によって再利用のためのIDを変えること
+  static const id identifiers[3] = { @"image-cell", @"switch-cell", @"slider-cell" };
+  NSString* identifier = identifiers[ indexPath.section ];
   UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:identifier];
   if ( nil == cell ) {
-    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                  reuseIdentifier:identifier];
-    [cell autorelease];
+    switch ( indexPath.section ) {
+      case 0:
+        cell = [[[CellWithImageView alloc] initWithReuseIdentifier:identifier] autorelease];
+        break;
+      case 1:
+        cell = [[[CellWithSwitch alloc] initWithReuseIdentifier:identifier] autorelease];
+        break;
+      case 2:
+      default:
+        cell = [[[CellWithSlider alloc] initWithReuseIdentifier:identifier] autorelease];
+        [(CellWithSlider*)cell setDelegate:self];
+        break;
+    }
   }
-  cell.textLabel.text =
-    [[dataSource_ objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
   switch ( indexPath.section ) {
-    case 0: //< セルにUIImageViewを追加
-      [cell.contentView addSubview:[self imageViewForCell:cell withFileName:@"Samurai.png"]];
+    case 0:
+      [[cell imageView] setImage:[UIImage imageNamed:@"Samurai.png"]];
       break;
-    case 1: //< セルにUISwitchを追加
-      [cell.contentView addSubview:[self switchForCell:cell]];
-      break;
-    case 2: //< セルにUISliderを追加
-      [cell.contentView addSubview:[self sliderForCell:cell]];
+    case 2:
+      {
+        // UISlider付のセルなら値を設定
+        NSNumber* value = [self.sliderValues objectAtIndex:indexPath.row];
+        CellWithSlider* cellWithSlider = (CellWithSlider*)cell;
+        cellWithSlider.slider.value = [value floatValue];
+        cellWithSlider.row = indexPath.row;
+      }
       break;
     default:
       break;
   }
+  cell.textLabel.text =
+    [[self.dataSource objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
   return cell;
 }
 
@@ -87,44 +223,10 @@
 
 #pragma mark ----- Private Methods -----
 
-- (UIImageView*)imageViewForCell:(const UITableViewCell*)cell withFileName:(NSString*)fileName {
-  UIImage* image = [UIImage imageNamed:fileName];
-  UIImageView* theImageView = [[[UIImageView alloc] initWithImage:image] autorelease];
-  CGPoint newCenter = cell.contentView.center;
-  newCenter.x += 80;
-  theImageView.center = newCenter;
-  theImageView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
-                                  UIViewAutoresizingFlexibleRightMargin |
-                                  UIViewAutoresizingFlexibleTopMargin |
-                                  UIViewAutoresizingFlexibleBottomMargin;
-  return theImageView;
-}
-
-- (UISwitch*)switchForCell:(const UITableViewCell*)cell {
-  UISwitch* theSwitch = [[[UISwitch alloc] init] autorelease];
-  theSwitch.on = YES;
-  CGPoint newCenter = cell.contentView.center;
-  newCenter.x += 80;
-  theSwitch.center = newCenter;
-  theSwitch.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
-                               UIViewAutoresizingFlexibleRightMargin |
-                               UIViewAutoresizingFlexibleTopMargin |
-                               UIViewAutoresizingFlexibleBottomMargin;
-  return theSwitch;
-}
-
-- (UISlider*)sliderForCell:(const UITableViewCell*)cell {
-  UISlider* theSlider = [[[UISlider alloc] init] autorelease];
-  theSlider.value = theSlider.maximumValue / 2;
-  theSlider.frame = CGRectMake( 0, 0, cell.bounds.size.width / 2, cell.bounds.size.height );
-  CGPoint newCenter = cell.contentView.center;
-  newCenter.x += 50;
-  theSlider.center = newCenter;
-  theSlider.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
-                               UIViewAutoresizingFlexibleRightMargin |
-                               UIViewAutoresizingFlexibleTopMargin |
-                               UIViewAutoresizingFlexibleBottomMargin;
-  return theSlider;
+- (void)slider:(UISlider*)slider valueDidChange:(float)value forRow:(NSUInteger)row {
+  // 変更された値を保存
+  [self.sliderValues replaceObjectAtIndex:row
+                               withObject:[NSNumber numberWithFloat:value]];
 }
 
 @end
